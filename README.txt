5ecf38f2095281648ed07f6bba565f35375deca7
VILLARD Lorenzo <villard.lorenzo.comptes@protonmail.com>
2020-05-28 15:15:06 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+Traing based on Udemy Course "Python Digital Image Processing From Ground Up"
---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name = 'Image Processing',
+    version='0.1dev',
+    url = '',
+    packages=[],
+    author='VILLARD Lorenzo',
+    author_email='villard.lorenzo.comptes@protonmail.com',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    long_description=open('README.txt').read()
+)
